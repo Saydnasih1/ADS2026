edb5149a07a94fe7414522e26c6feae80dbea0f9
rishinmh <therealrishin@gmail.com>
2026-09-16 12:05:25 +0530

    add avg.c

---
+++ avg.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+  int marks[10],i,n,sum=0;
+  double avg;
+  printf("Enter number of elements: ");
+  scanf("%d",&n);
+  for(i=0;i<n;++i)
+  {
+   printf("Enter number %d:",i+1);
+   scanf("%d",&marks[i]);
+   sum+=marks[i];
+  }
+ avg=(double) sum/n;
+ printf("Average=%.2lf\n",avg);
+ return 0;
+
+}
